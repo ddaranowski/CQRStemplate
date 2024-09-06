0f12012f3ee4e66f804d92830d731f6a7a64c2af
Damian Daranowski <damian.daranowski@stralfors.se>
2024-09-06 13:42:52 +0200

    Add Profile for ger

--- CQRSFluentAndAutomapper/Application/Flows/Queries/GetFlowByIdHandler.cs
+++ CQRSFluentAndAutomapper/Application/Flows/Queries/GetFlowByIdHandler.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using CQRSMediatR.Api.DTOs;
 using CQRSMediatR.Api.Services;
 using MediatR;
@@ -7,10 +8,12 @@ namespace CQRSMediatR.Api.Application.Flows.Queries;
 public class GetFlowByIdHandler : IRequestHandler<GetFlowByIdRequest, FlowDto>
 {
     private readonly IFlowService _flowService;
+    private readonly IMapper _mapper;
 
-    public GetFlowByIdHandler(IFlowService flowService)
+    public GetFlowByIdHandler(IFlowService flowService, IMapper mapper)
     {
         _flowService = flowService;
+        _mapper = mapper;
     }
 
     public async Task<FlowDto> Handle(GetFlowByIdRequest request, CancellationToken cancellationToken)
@@ -20,17 +23,20 @@ public class GetFlowByIdHandler : IRequestHandler<GetFlowByIdRequest, FlowDto>
         if (flow == null)
             return null;
 
-        return new FlowDto
-        {
-            Name = flow.Name,
-            ApplicationId = flow.ApplicationId,
-            Channels = flow.Channels.Select(channel => new ChannelDto
-            {
-                ChannelType = channel.ChannelType,
-                Country = channel.Country,
-                Priority = channel.Priority
-            }).ToList()
-        };
+        // Use AutoMapper to map Flow to FlowDto
+        return _mapper.Map<FlowDto>(flow);
+
+        //return new FlowDto
+        //{
+        //    Name = flow.Name,
+        //    ApplicationId = flow.ApplicationId,
+        //    Channels = flow.Channels.Select(channel => new ChannelDto
+        //    {
+        //        ChannelType = channel.ChannelType,
+        //        Country = channel.Country,
+        //        Priority = channel.Priority
+        //    }).ToList()
+        //};
     }
 
 
